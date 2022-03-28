94777d2e5f8d68f4d4b7e32325f756b94337b587
Arr-Dee <russeljames@hotmail.co.uk>
2022-03-28 18:17:08 +0100

    Initial

---
+++ app.py
@@ -0,0 +1,169 @@
+from flask import Flask, redirect, make_response, render_template, request, session
+from cs50 import SQL
+from flask_session.__init__ import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+from helpers import calculate_score, password_check
+import json
+
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+db = SQL("sqlite:///database.db")
+
+@app.route("/")
+def home():
+    
+    return render_template("home.html")
+
+
+@app.route("/comp/<comp_id>", methods=['POST', 'GET'])
+def comp(comp_id):
+    info = db.execute("SELECT * FROM comps WHERE comp = ?", comp_id)[0]
+
+    comp = comp_id
+    no_climbs = info["climbs"]
+    zone = info["zone"]
+    top = info["top"]
+
+    if request.method == 'POST':
+            climb_data = []
+
+            for n in range(1,no_climbs+1):
+                climb_data.append(request.form.get(f'climb-{n}'))
+
+            # If User is logged in
+            if session:
+                #print("Logged in.")
+
+                # If User already has a saved list of results  
+                if db.execute("SELECT * FROM results WHERE user_id = ? AND comp = ?", session["user_id"], comp):
+                    db.execute("UPDATE results SET results = ? WHERE user_id = ? AND comp = ?", json.dumps(climb_data), session["user_id"],comp)
+                else:
+                    db.execute("INSERT INTO results (user_id, comp, results) VALUES(?, ?, ?)", session["user_id"], comp, json.dumps(climb_data))
+
+                results = json.loads(db.execute("SELECT results FROM results WHERE user_id = ? AND comp = ?", session["user_id"], comp)[0]["results"])
+            else:
+                results = climb_data
+
+            # Calculate score
+            score = calculate_score(climb_data,zone,top)
+
+            resp = make_response(render_template('comp.html', comp=comp, no_climbs=no_climbs, score=score, results=results))
+
+            # Set cookies
+            resp.set_cookie(f'{comp}_score', str(score))
+            resp.set_cookie(f'{comp}_data', json.dumps(results))
+            
+            return resp
+
+    else:
+
+        # Get data from cookies
+        score = request.cookies.get(f'{comp}_score')
+
+        if session:
+            data = db.execute("SELECT * FROM results WHERE user_id = ? AND comp = ?", session["user_id"], comp)
+            if data:
+                results = json.loads(data[0]["results"])
+            else:
+                results = []
+        elif f'{comp}_data' in request.cookies:
+            results = json.loads(request.cookies.get(f'{comp}_data'))
+        else:
+            results = []
+
+        return render_template("comp.html", comp=comp, no_climbs=no_climbs, score=score, results=results)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    if request.method == "GET":
+        return render_template("register.html")
+
+    else:
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        res = password_check(password)
+        print(res)
+
+        if not all(res.values()):
+            return render_template("register.html", error="Passwords do not meet specified requirements.")
+
+        confirmation = request.form.get("confirmation")
+
+        if not username:
+            return render_template("register.html", error="No username provided.")
+
+        if not password:
+            return render_template("register.html", error="No password provided.")
+
+        if password != confirmation:
+            return render_template("register.html", error="Password do not match.")
+
+        other_users = db.execute("SELECT * FROM users WHERE username = ?", username)
+        if other_users:
+            return render_template("register.html", error="Username already in use.")
+
+        db.execute("INSERT INTO users (username, hash) VALUES(?, ?)", username, generate_password_hash(password))
+        return render_template("login.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return render_template("login.html", error="No username provided.")
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return render_template("login.html", error="No password provided.")
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return render_template("login.html", error="Invalid username and/or password.")
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+        session["username"] = rows[0]["username"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+
+if __name__ == "__main__":
+  app.run()
